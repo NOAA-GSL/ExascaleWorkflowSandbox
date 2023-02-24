82902980b3c0896b64e64fc4a6f6a312cdfad38e
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2023-02-24 11:17:27 -0600

    Change specification of Spack install directory
    The user no specifies the directory of the spack
    install rather than the parent directory of the install.

--- ExaWorks/scripts/install_spack.sh
+++ ExaWorks/scripts/install_spack.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash                                                                                                                                                                                                              
+
 ################################################################################
 # help                                                                         #
 ################################################################################
@@ -28,19 +29,19 @@ if [[ -d ~/.spack ]]; then
 fi
 
 # Check for existing Spack installation
-if [[ -d ${SPACK_DIR}/spack ]]; then
-  echo "A Spack installation already exists at ${SPACK_DIR}/spack."
+if [[ -d ${SPACK_DIR} ]]; then
+  echo "A Spack installation already exists at ${SPACK_DIR}."
   echo
-  echo "Please choose another location or remove ${SPACK_DIR}/spack and try again."
+  echo "Please choose another location or remove ${SPACK_DIR} and try again."
   exit 1
 fi
 
-# Create empty installation directory and remove any previous configuration
+# Create empty installation directory
 [[ -d ${SPACK_DIR} ]] || mkdir -p ${SPACK_DIR}
 
 # Install Spack via repository clone
-git clone https://github.com/spack/spack.git ${SPACK_DIR}/spack
+git clone https://github.com/spack/spack.git ${SPACK_DIR}
 chmod -fR 02770 ${SPACK_DIR}/spack || true
-. ${SPACK_DIR}/spack/share/spack/setup-env.sh
+. ${SPACK_DIR}/share/spack/setup-env.sh
 
 exit 0
