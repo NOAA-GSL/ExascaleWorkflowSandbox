e24f8dd25b10ca41ccf7778ed67fa0f8fc83c9c2
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2023-03-08 16:07:40 -0700

    Add ExaWorks Docker containers and update ExaWorks install scripts (#18)
    * Add ExaWorks Docker containers and update ExaWorks install scripts
    
    Dockerfiles for both Ubuntu and CentOS containers is added.
    Scripts for installing Spack and Exaworks are updated.
    Add basic "hello" Parsl test script.
    Modify CI workflow to use containers for testing ExaWorks.
    
    * Rename CI workflow
    
    Modify CI workflow names to be consistent with current usage.
    
    * Fix typo in comment and add newline at end of Docker files

---
+++ ExaWorks/test/parsl_hello.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+import os
+
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=CryptographyDeprecationWarning)
+    import parsl
+    from parsl.app.app import python_app, bash_app
+    from parsl.configs.local_threads import config
+
+# Print the version of Parsl we are using
+print(f"Running Parsl version: {parsl.__version__}")
+
+# Configure Parsl with default config
+parsl.load(config)
+
+# Define a "hello world" Python Parsl App
+@python_app
+def python_hello ():
+    return 'Hello World! from Python App'
+
+
+# Define a "hello world" Bash Parsl App
+@bash_app
+def bash_hello(stdout='echo-hello.stdout', stderr='echo-hello.stderr'):
+    return 'echo "Hello World! from Bash App"'
+
+# Run the Python "hello world", wait for the result, and print it
+print(python_hello().result())
+
+# Run the Bash "hello world", wait for the result, and print it
+bash_hello().result()
+with open('echo-hello.stdout', 'r') as f:
+     print(f.read())
