af9ed309b42607e2e36f7f07b89449ee917b1990
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2023-03-28 09:52:14 -0600

    Add CI test for Docker container  (#20)
    * Add CI test for Docker container and update checkout action for test example CI
    
    Also, add py-pylint to the Spack install script to enable better testing.
    
    * Restructure ExaWorks installation into small pieces
    
    Create installation scripts for each main ExaWorks component.
    Create Docker files for each main ExaWorks component.
    Use Spack build caches to optimize installation times.
    Update Github Actions workflow to leverage Spack build caches.
    Update Github Actions workflow to split install into different build jobs.
    
    * Add missing colon from workflow CI yml file
    
    * Fix another missing colon in the workflow yaml file
    
    * Rework Docker containers and install scripts
    
    Install scripts and containers were modified to build
    and use a "bootstrap" compiler for building the exaworks
    software.  This is necessary because dependencies shared
    between various components of exaworks will induce multiple
    rebuilds of the gcc compiler, which takes a very long time.
    This is avoided by bootstrapping gcc with the native
    compiler, and then rebuilding the newer gcc with itself
    in a "base" environment.  In this way, gcc is built twice
    for the base environment, but subsequent environments
    using the "base" environment will not rebuild gcc.
    
    This commit also adds CentOS7 containers for the
    multi-stage build of exaworks that breaks it into
    smaller pieces with shorter build times. The Github
    Actions workflow was updated to add CentOS containers.
    
    * Fix typo in script comment

--- ExaWorks/scripts/install_base.sh
+++ ExaWorks/scripts/install_base.sh
@@ -4,8 +4,8 @@
 DEFAULT_GCC_VERSION=$(gcc --version | head -1 | sed -e 's/([^()]*)//g' | awk '{print $2}')  # Version of system default gcc
 DEFAULT_COMPILER="gcc@${DEFAULT_GCC_VERSION}"  # Default system compiler used to build newer gcc
 
-SPACK_ENV_NAME="exaworkssdk"     # Name of spack environment for ExaWorks SDK
-SPACK_ENV_COMPILER="gcc@9.4.0"   # Compiler to use to build ExaWorks  SDK
+SPACK_ENV_NAME="base"            # Name of spack environment to create
+SPACK_ENV_COMPILER="gcc@9.4.0"   # Compiler to use to build the spack environment
 TARGET_ARCH_OPT="target=x86_64"  # Compiler architecture build target
 
 ################################################################################
@@ -13,11 +13,12 @@ TARGET_ARCH_OPT="target=x86_64"  # Compiler architecture build target
 ################################################################################
 help()
 {
-   # Display help
-   echo "Installs exaworks into exaworkssdk Spack environment"
-   echo
-   echo "Usage: install_exaworks.sh"
-   echo
+  # Display help
+  echo "Installs a base Spack environment to use for building exaworks packages." 
+  echo "This includes: gcc, python, pytest, pylint, flake8, mypy, and black."
+  echo
+  echo "Usage: install_base.sh"
+  echo
 }
 
 # Get the location of Spack so we can update permissions
@@ -39,16 +40,29 @@ spack config add concretizer:reuse:true
 spack config add config:db_lock_timeout:300
 spack config add config:install_tree:padded_length:128
 
-# Load the base compiler (this assumes ./install_base.sh has already been run)
-spack compiler add $(spack location -i ${SPACK_ENV_COMPILER}%${SPACK_ENV_COMPILER})
+# Create a compiler bootstrap environment
+spack env create bootstrap || true
+spack env activate bootstrap
+spack compiler find
 
-# Configure spack environment
+# Install bootstrap $COMPILER in bootstrap environment
+spack add ${SPACK_ENV_COMPILER} %${DEFAULT_COMPILER} ${TARGET_ARCH_OPT}
+spack install
+
+# Get the location of the bootstrap compiler
+BOOTSTRAP_COMPILER=$(spack location -i ${SPACK_ENV_COMPILER})
+
+# Create the base environment
 spack env create ${SPACK_ENV_NAME} || true
 spack env activate ${SPACK_ENV_NAME}
+spack compiler add ${BOOTSTRAP_COMPILER}
 
-# Install $COMPILER in Spack environment
-spack add ${SPACK_ENV_COMPILER} %${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
+# Use the boostrap compiler to install the compiler in the base environment
+spack add ${SPACK_ENV_COMPILER}%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
+spack concretize
 spack install
+spack compiler remove ${SPACK_ENV_COMPILER}%${DEFAULT_COMPILER}
+spack compiler add $(spack location -i ${SPACK_ENV_COMPILER}%${SPACK_ENV_COMPILER})
 
 # Install python tools
 spack add python@3.9%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
@@ -57,11 +71,17 @@ spack add py-pylint%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
 spack add py-flake8%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
 spack add py-mypy%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
 spack add py-black%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
+spack concretize
 spack install
 
-# Install exaworks
-spack add exaworks@0.1.0%${SPACK_ENV_COMPILER} ^python@3.9 ${TARGET_ARCH_OPT}
-spack concretize -f
-spack install
+# Remove bootstrap environment
+spack env deactivate
+if [ "${SPACK_ENV_COMPILER}" != "${DEFAULT_COMPILER}" ]; then
+  spack env activate bootstrap
+  spack uninstall -y ${SPACK_ENV_COMPILER}%${DEFAULT_COMPILER}
+  spack gc -y
+  spack env deactivate
+fi
+spack env remove -y bootstrap
 
 exit 0
