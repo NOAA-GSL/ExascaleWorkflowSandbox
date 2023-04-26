463d135062bf9a777b2b86a284e18d9ce704cf1a
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2023-04-26 08:10:15 -0600

    Separate bootstrap and base installation (#24)
    * Split install of bootstrap compiler into its own script
    
    Do not remove compiler bootstrap environment because its needed
    when installing from a build cache.
    
    Add explicit specs for compilers to disambiguate.
    
    * Update Dockerfiles for Spack exaworks base install
    
    * Fix accidental incorrect placement of ./install_bootstrap.sh in Dockerfile
    
    * Fix comment "boostrap" typos

--- ExaWorks/scripts/install_bootstrap.sh
+++ ExaWorks/scripts/install_bootstrap.sh
@@ -14,10 +14,11 @@ TARGET_ARCH_OPT="target=x86_64"  # Compiler architecture build target
 help()
 {
   # Display help
-  echo "Installs a base Spack environment to use for building exaworks packages." 
-  echo "This includes: gcc, python, pytest, pylint, flake8, mypy, and black."
+  echo "Installs a bootstrap compiler built by the default system compiler into a bootstrap environment."
+  echo "This bootstrap compiler can then be used to build other packages without incurring"
+  echo "dependencies on the system default compiler."
   echo
-  echo "Usage: install_base.sh"
+  echo "Usage: install_bootstrap.sh"
   echo
 }
 
@@ -40,28 +41,13 @@ spack config add concretizer:reuse:true
 spack config add config:db_lock_timeout:300
 spack config add config:install_tree:padded_length:128
 
-# Get the location of the bootstrap compiler
-BOOTSTRAP_COMPILER=$(spack location -i ${SPACK_ENV_COMPILER}%${DEFAULT_COMPILER})
+# Create a compiler bootstrap environment
+spack env create bootstrap || true
+spack env activate bootstrap
+spack compiler find
 
-# Create the base environment
-spack env create ${SPACK_ENV_NAME} || true
-spack env activate ${SPACK_ENV_NAME}
-spack compiler add ${BOOTSTRAP_COMPILER}
-
-# Use the bootstrap compiler to install the compiler in the base environment
-spack add ${SPACK_ENV_COMPILER}%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
-spack concretize
-spack install
-spack compiler add $(spack location -i ${SPACK_ENV_COMPILER}%${SPACK_ENV_COMPILER})
-
-# Install python tools
-spack add python@3.9%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
-spack add py-pytest%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
-spack add py-pylint%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
-spack add py-flake8%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
-spack add py-mypy%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
-spack add py-black%${SPACK_ENV_COMPILER} ${TARGET_ARCH_OPT}
-spack concretize
+# Install bootstrap $COMPILER in bootstrap environment
+spack add ${SPACK_ENV_COMPILER}%${DEFAULT_COMPILER} ${TARGET_ARCH_OPT}
 spack install
 
 exit 0
