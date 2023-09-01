9237d1a5b7af6e206c7edd667b546ce1e5c15ed8
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2023-09-01 11:08:39 -0600

    Reorganize tests to compare output against baselines (#32)
    * Reorganize tests to compare output against baselines
    
    * Fix typo in CI yaml config
    
    * Fix typo in CI test yaml
    
    * Fix incorrect python for installing parsl via pip
    
    * Fix parsl_flux_resource_list baseline for CI resources

--- docker/test/src/parsl_hello.py
+++ docker/test/src/parsl_hello.py
@@ -13,20 +13,21 @@ parsl.load(config)
 
 # Define a "hello world" Python Parsl App
 @python_app
-def python_hello ():
+def python_hello (stdout=None, stderr=None):
     return 'Hello World! from Python App'
-
 
 # Define a "hello world" Bash Parsl App
 @bash_app
-def bash_hello(stdout='echo-hello.stdout', stderr='echo-hello.stderr'):
+def bash_hello(stdout=None, stderr=None):
     return 'echo "Hello World! from Bash App"'
 
-# Run the Python "hello world", wait for the result, and print it
-print(python_hello().result())
+# Run the Python "hello world", wait for the result, and write it to output file
+with open('parsl_python_hello.out', 'w') as f:
+    f.write(f"{python_hello().result()}\n")
 
-# Run the Bash "hello world", wait for the result, and print it
-bash_hello().result()
-with open('echo-hello.stdout', 'r') as f:
-     print(f.read())
+# Run the Bash "hello world", wait for the result, and write it to output file
+bash_hello(stdout=('parsl_bash_hello.out', 'w'),
+           stderr=('parsl_bash_hello.err', 'w')).result()
 
+# Cleanup
+parsl.clear()
