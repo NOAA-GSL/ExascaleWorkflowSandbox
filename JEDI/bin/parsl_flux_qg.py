95582363f9eda2891d0c9a25c7a539f1e6b535d7
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2023-09-20 12:23:58 -0500

    Add truth, obs, and assimilaiton steps to Parsl workflow

--- JEDI/bin/parsl_flux_qg.py
+++ JEDI/bin/parsl_flux_qg.py
@@ -1,5 +1,11 @@
 #!/bin/env python3
 
+import sys
+import yaml
+from datetime import datetime, timedelta
+
+import leadtime
+
 import parsl
 from parsl.app.app import python_app, bash_app
 from parsl.config import Config
@@ -55,42 +61,71 @@ def resource_list():
 @bash_app
 def truth_app(jedi_path, stdout=None, stderr=None, parsl_resource_specification={}):
     return '''
-    . {}/bin/setupenv-hercules.sh
+    export JEDI_PATH={}
+    . $JEDI_PATH/bin/setupenv-hercules.sh
     unset I_MPI_PMI_LIBRARY
-    {}/bin/makeTruth.py {}/yaml/qg_experiment.yaml
-    mkdir -p {}/experiments/qg.3dvar/truth
-    cd {}/experiments/qg.3dvar/truth
-    ../../../exascale-workflow-bundle/bin/qg_forecast.x ../yaml/truth.yaml
-    '''.format(jedi_path, jedi_path, jedi_path, jedi_path, jedi_path)
+    $JEDI_PATH/bin/makeTruth.py $JEDI_PATH/yaml/qg_experiment.yaml
+    mkdir -p $JEDI_PATH/experiments/qg.3dvar/truth
+    cd $JEDI_PATH/experiments/qg.3dvar/truth
+    $JEDI_PATH/exascale-workflow-bundle/bin/qg_forecast.x $JEDI_PATH/experiments/qg.3dvar/yaml/truth.yaml
+    '''.format(jedi_path)
 
 # Create simulate obs
 @bash_app
-def obs_app(jedi_path, stdout=None, stderr=None, parsl_resource_specification={"num_tasks": 1}):
+def obs_app(jedi_path, t, assimilation_type, stdout=None, stderr=None, parsl_resource_specification={}):
     return '''
-    #. {}/bin/setupenv-hercules.sh
-    #unset I_MPI_PMI_LIBRARY
-    {}/bin/makeObs.py {}/yaml/qg_experiment.yaml
-    '''.format(jedi_path, jedi_path, jedi_path)
+    export JEDI_PATH={}
+    export ANALYSIS_TIME={}
+    export TYPE={}
+    . $JEDI_PATH/bin/setupenv-hercules.sh
+    unset I_MPI_PMI_LIBRARY
+    $JEDI_PATH/bin/makeOb.py $JEDI_PATH/yaml/qg_experiment.yaml $ANALYSIS_TIME
+    mkdir -p $JEDI_PATH/experiments/qg.3dvar/obs
+    cd $JEDI_PATH/experiments/qg.3dvar/obs
+    $JEDI_PATH/exascale-workflow-bundle/bin/qg_hofx.x $JEDI_PATH/experiments/qg.3dvar/yaml/makeobs.$TYPE.$ANALYSIS_TIME.yaml
+    '''.format(jedi_path, t, assimilation_type)
 
-## Compile the hello MPI program with Intel
-#@bash_app
-#def compile_app(dirpath, stdout=None, stderr=None, compiler="mpiifort -fc=ifx", parsl_resource_specification={}):
-#    return '''
-#    module load intel-oneapi-compilers/2022.2.1 intel-oneapi-mpi/2021.7.1
-#    cd {}
-#    {} -o mpi_hello.exe mpi_hello.f90
-#    '''.format(dirpath, compiler)
 
-# Run the hello MPI program with Intel
-#@bash_app
-#def mpi_hello(dirpath, stdout=None, stderr=None, app="./mpi_hello.exe", parsl_resource_specification={}):
-#    return '''
-#    # Flux requires I_MPI_PMI_LIBRARY to be unset because it provides its own PMI
-#    module load intel-oneapi-compilers/2022.2.1 intel-oneapi-mpi/2021.7.1
-#    unset I_MPI_PMI_LIBRARY
-#    cd {}
-#    {}
-#    '''.format(dirpath, app)
+# Run assimilation
+@bash_app
+def obs_app(jedi_path, t, assimilation_type, stdout=None, stderr=None, parsl_resource_specification={}):
+    return '''
+    export JEDI_PATH={}
+    export ANALYSIS_TIME={}
+    export TYPE={}
+    export ALGORITHM={}
+    . $JEDI_PATH/bin/setupenv-hercules.sh
+    unset I_MPI_PMI_LIBRARY
+    $JEDI_PATH/bin/runAssimilation.py $JEDI_PATH/yaml/qg_experiment.yaml $ANALYSIS_TIME
+    cd $JEDI_PATH/experiments/qg.3dvar/forecasts/$ANALYSIS_TIME/on
+    $JEDI_PATH/exascale-workflow-bundle/bin/qg_4dvar.x $JEDI_PATH/experiments/qg.3dvar/yaml/$TYPE.$ALGORITHM.$ANALYSIS_TIME.yaml
+    '''.format(jedi_path, t, assimilation_type, assimilation_algorithm)
+
+#############################
+#
+# Begin workflow program
+#
+#############################
+
+# Get experiment yaml file path from comand line
+exp_config_file = sys.argv[1]
+
+# Extract path to experiment configuration
+exp_config_path = os.path.dirname(os.path.abspath(exp_config_file))
+
+# Load the experiment configuration
+with open(exp_config_file, 'r') as file:
+    exp_config = yaml.safe_load(file)
+
+# Get the driver path
+driver_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+# Get the experiment forecast time range parameters
+exp_start_str = exp_config['experiment']['begin']
+exp_start_time = datetime.strptime(exp_start_str, "%Y-%m-%dT%H:%M:%SZ")
+exp_length = leadtime.fcst_to_seconds(exp_config['experiment']['length'])
+exp_freq = leadtime.fcst_to_seconds(exp_config['experiment']['cycle frequency'])
+exp_end = exp_start_time + timedelta(0, exp_length) - timedelta(0, exp_freq)
 
 # Check the Flux resource list
 r = resource_list().result()
@@ -102,37 +137,40 @@ truth = truth_app(jedi_path='/work/noaa/gsd-hpcs/charrop/hercules/SENA/ExascaleW
                   parsl_resource_specification={"num_tasks": 1, "num_nodes": 1}
                   ).result()
 
-# Create the simulated observations
-#obs = obs_app(jedi_path='/work/noaa/gsd-hpcs/charrop/hercules/SENA/ExascaleWorkflowSandbox/JEDI',
-#              stdout=os.path.join(jedi_path, 'makeobs.out'),
-#              stderr=os.path.join(jedi_path, 'makeobs.err')
-#              ).result()
+# Run experiment cycles
+t = exp_start_time
+while t <= exp_end:
 
-# Run cycled experiment
-# t=0
-# while t < exp_end_date:
-# .....
-#   Run assimilation
-#   Run forecast
-#
+    t_str = t.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-# Run verification
+    print(f'Running cycle {t_str}')
 
+    # Run the assimilation if needed
+    if t > exp_start_time:
 
+        # Create simulated obs for this analysis time
+        obs = obs_app(jedi_path='/work/noaa/gsd-hpcs/charrop/hercules/SENA/ExascaleWorkflowSandbox/JEDI',
+                      t=t_str,
+                      assimilation_type='3dvar',
+                      stdout=os.path.join(jedi_path, 'obs.out'),
+                      stderr=os.path.join(jedi_path, 'obs.err'),
+                      parsl_resource_specification={"num_tasks": 1, "num_nodes": 1}
+                      ).result()
 
-# complile the app and wait for it to complete (.result())
-#compile_app(dirpath=shared_dir,
-#            stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.out"),
-#            stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.err"),
-#           ).result()
-#
-## run the mpi app
-#hello = mpi_hello(dirpath=shared_dir,
-#                  stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.out"),
-#                  stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.err",),
-#                  parsl_resource_specification={"num_tasks": 120, "num_nodes": 3})
-#
-## Wait for the MPI app to finish
-#hello.result()
+        # Run the assimilation
+        3dvar = obs_app(jedi_path='/work/noaa/gsd-hpcs/charrop/hercules/SENA/ExascaleWorkflowSandbox/JEDI',
+                        t=t_str,
+                        assimilation_type='3dvar',
+                        assimilation_algorithm='dripcg',
+                        stdout=os.path.join(jedi_path, '3dvar.out'),
+                        stderr=os.path.join(jedi_path, '3dvar.err'),
+                        parsl_resource_specification={"num_tasks": 1, "num_nodes": 1}
+                        ).result()
+
+    # Run the forecast
+    #subprocess.run([f'{driver_path}/runForecast.py', f'{exp_config_file}', f'{t_str}'], stdout = sys.stdout, stderr = sys.stdout)
+
+    # Increment cycle
+    t = t + timedelta(0, exp_freq)
 
 parsl.clear()
