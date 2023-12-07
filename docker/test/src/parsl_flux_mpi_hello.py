48538aa56bcf7a51a2096eece0480fd8af1b01d7
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2023-12-07 08:52:45 -0700

    Move configuration into platform YAML files (#40)
    * Small fixes to conda environment and creation of machine-specific stacks
    
    * Update chiltepin stack for container use
    
    * Create a rudimentary stack factory
    
    * Add rudimentary factory for config
    
    * Move config to YAML files
    
    * Update CI to use yaml file argument and remove obsolete JEDI stack calls
    
    * Update baseline test data to match change in test code

--- docker/test/src/parsl_flux_mpi_hello.py
+++ docker/test/src/parsl_flux_mpi_hello.py
@@ -2,133 +2,85 @@
 
 import parsl
 from parsl.app.app import python_app, bash_app
-from parsl.config import Config
-from parsl.channels import LocalChannel
-from parsl.providers import SlurmProvider
-from parsl.executors import FluxExecutor
-from parsl.launchers import SimpleLauncher
-from parsl.addresses import address_by_hostname
-from parsl.data_provider.files import File
-
-# Update to import config for your machine
-config = Config(
-    executors=[
-        FluxExecutor(
-            label="flux",
-            # Start Flux with srun and tell it there are 40 cores per node
-            launch_cmd='srun --mpi=pmi2 --tasks-per-node=1 -c2 ' + FluxExecutor.DEFAULT_LAUNCH_CMD,
-            provider=SlurmProvider(
-                channel=LocalChannel(),
-                nodes_per_block=3,
-                init_blocks=1,
-                partition='slurmpar',
-                account='',
-                walltime='00:10:00',
-                launcher=SimpleLauncher(),
-                worker_init='''
-source /opt/conda_init.sh
-conda activate chiltepin 
-''',
-            ),
-        )
-    ],
-)
-
 
 import os
+import sys
+import yaml
 
+import chiltepin_config
+
+# Open and parse the yaml config
+with open(sys.argv[1], "r") as stream:
+    try:
+        yaml_config = yaml.safe_load(stream)
+    except yaml.YAMLError as e:
+        print("Invalid taml configuration")
+        raise(e)
+
+# Load the configuration
+config, env_init = chiltepin_config.config_factory(yaml_config)
 parsl.load(config)
 
-remote = False
 shared_dir = './'
 
-chiltepin= '''
-    . /opt/conda_init.sh
-    conda activate chiltepin
-'''
-
-spack_stack='''
-    . /opt/jedi_init.sh
-'''
+stack = env_init
 
 # Print out resources that Flux sees after it starts
 @bash_app
-def resource_list():
+def resource_list(stack=stack):
     return '''
-    . /opt/conda_init.sh
-    conda activate chiltepin
+    {}
     flux resource list > parsl_flux_resource_list.txt
-    '''
+    '''.format(stack)
 
 # Test Flux PMI launch
 @bash_app
-def pmi_barrier(parsl_resource_specification={}):
+def pmi_barrier(stack=stack, parsl_resource_specification={}):
     return '''
-    . /opt/conda_init.sh
-    conda activate chiltepin
-    env | grep -i flux > parsl_flux_pmi_barrier.txt
-    env | grep -i pmi >> parsl_flux_pmi_barrier.txt
-    export FLUX_PMI_LIBRARY_PATH=/opt/miniconda3/envs/chiltepin/lib/flux/libpmi.so
-    flux pmi --method=libpmi:$FLUX_PMI_LIBRARY_PATH barrier >> parsl_flux_pmi_barrier.txt
-    '''
+    {}
+    flux pmi barrier > parsl_flux_pmi_barrier.txt
+    '''.format(stack)
 
 # Compile the hello MPI program with GNU and OpenMPI
 @bash_app
-def compile_app(dirpath, stdout=None, stderr=None, stack=chiltepin, parsl_resource_specification={"num_tasks": 1}):
+def compile_app(dirpath, stdout=None, stderr=None, stack=stack, parsl_resource_specification={"num_tasks": 1}):
     return '''
     {}
     cd {}
-    mpif90 -o mpi_hello.exe mpi_hello.f90
+    $CHILTEPIN_MPIF90 -o mpi_hello.exe mpi_hello.f90
     '''.format(stack,dirpath)
 
 # Run the hello MPI program with GNU and OpenMPI
 @bash_app
-def mpi_hello(dirpath, stdout=None, stderr=None, stack=chiltepin, parsl_resource_specification={}):
+def mpi_hello(dirpath, stdout=None, stderr=None, stack=stack, parsl_resource_specification={}):
     return '''
     {}
-    export FLUX_PMI_LIBRARY_PATH=/opt/miniconda3/envs/chiltepin/lib/flux/libpmi.so
     cd {}
     ./mpi_hello.exe
     '''.format(stack, dirpath)
 
 # Check the Flux resource list
-r = resource_list().result()
+r = resource_list(stack=stack).result()
 
 # Check the Flux pmi status
-p = pmi_barrier(parsl_resource_specification={"num_tasks": 6, "num_nodes": 3}).result()
+p = pmi_barrier(stack=stack, parsl_resource_specification={"num_tasks": 6, "num_nodes": 3}).result()
 
 # complile the app with chiltepin stack and wait for it to complete (.result())
 compile_app(dirpath=shared_dir,
             stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.out"),
             stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.err"),
-            stack=chiltepin,
+            stack=stack,
            ).result()
 
 # run the mpi app with chiltepin stack
 hello = mpi_hello(dirpath=shared_dir,
                   stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.out"),
                   stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.err",),
-                  stack=chiltepin,
+                  stack=stack,
                   parsl_resource_specification={"num_tasks": 6, "num_nodes": 3})
 
 # Wait for the MPI app with chiltepin stack to finish
 hello.result()
 
-# complile the app with spack-stack stack and wait for it to complete (.result())
-compile_app(dirpath=shared_dir,
-            stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.out"),
-            stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.err"),
-            stack=spack_stack,
-           ).result()
-
-# run the mpi app with spack-stack stack
-hello = mpi_hello(dirpath=shared_dir,
-                  stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.out"),
-                  stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.err",),
-                  stack=spack_stack,
-                  parsl_resource_specification={"num_tasks": 6, "num_nodes": 3})
-
-# Wait for the MPI app with spack-stack stack to finish
-hello.result()
-
+# Cleanup
 parsl.clear()
