c06b2395338afc7470a8b517994b24d816978424
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2023-12-10 15:49:03 -0700

    Improve tests to check for output correctness

--- tests/test_parsl_flux_mpi_hello.py
+++ tests/test_parsl_flux_mpi_hello.py
@@ -2,6 +2,7 @@ import os
 import parsl
 from parsl.app.app import python_app, bash_app
 import pytest
+import re
 import yaml
 
 from chiltepin.config.factory import config_factory
@@ -19,18 +20,18 @@ def parse_config(filename):
 
 # Print out resources that Flux sees after it starts
 @bash_app
-def resource_list(env=""):
+def resource_list(stdout=None, stderr=None, env=""):
     return '''
     {}
-    flux resource list > parsl_flux_resource_list.txt
+    flux resource list
     '''.format(env)
 
 # Test Flux PMI launch
 @bash_app
-def pmi_barrier(env="", parsl_resource_specification={}):
+def pmi_barrier(stdout=None, stderr=None, env="", parsl_resource_specification={}):
     return '''
     {}
-    flux pmi barrier > parsl_flux_pmi_barrier.txt
+    flux pmi barrier
     '''.format(env)
 
 # Compile the hello MPI program with GNU and OpenMPI
@@ -62,26 +63,41 @@ def load_config(config_file, request):
 
 # Test Flux resource list
 def test_flux_resource_list(load_config):
-    r = resource_list(env=load_config).result()
+    r = resource_list(stdout=('parsl_flux_resource_list.out', 'w'),
+                      stderr=('parsl_flux_resource_list.err', 'w'),
+                      env=load_config).result()
+    assert r == 0
     
 # Test Flux pmi barrier
 def test_flux_pmi(load_config):
-    p = pmi_barrier(env=load_config, parsl_resource_specification={"num_tasks": 6, "num_nodes": 3}).result()
+    p = pmi_barrier(stdout=('parsl_flux_pmi_barrier.out', 'w'),
+                    stderr=('parsl_flux_pmi_barrier.err', 'w'),
+                    env=load_config,
+                    parsl_resource_specification={"num_tasks": 6, "num_nodes": 3},
+                    ).result()
+    assert p == 0
+    with open("parsl_flux_pmi_barrier.out", "r") as f:
+        assert re.match(r'ƒ\S+: completed pmi barrier on 6 tasks in [0-9.]+s.', f.read())
 
 def test_compile_app(load_config):
     shared_dir = "./"
-    compile_app(dirpath=shared_dir,
-                stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.out"),
-                stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.err"),
-                env=load_config,
-                ).result()
+    c = compile_app(dirpath=shared_dir,
+                    stdout=(os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.out"), 'w'),
+                    stderr=(os.path.join(shared_dir, "parsl_flux_mpi_hello_compile.err"), 'w'),
+                    env=load_config,
+                    ).result()
+    assert c == 0
+    assert os.path.isfile("mpi_hello.exe")
+    assert os.stat("parsl_flux_mpi_hello_compile.out").st_size == 0
 
 def test_mpi_hello(load_config):
     shared_dir = "./"
+    os.remove("parsl_flux_mpi_hello_run.out")
+    os.remove("parsl_flux_mpi_hello_run.err")
     hello = mpi_hello(dirpath=shared_dir,
                       stdout=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.out"),
-                      stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.err",),
+                      stderr=os.path.join(shared_dir, "parsl_flux_mpi_hello_run.err"),
                       env=load_config,
-                      parsl_resource_specification={"num_tasks": 6, "num_nodes": 3})
-    # Wait for the MPI app with chiltepin stack to finish
-    hello.result()
+                      parsl_resource_specification={"num_tasks": 6, "num_nodes": 3},
+                      ).result()
+    assert hello == 0
