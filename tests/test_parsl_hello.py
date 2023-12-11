e0615ef54e3df3f2a5a7e844f2ae10aa7228b611
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2023-12-11 11:51:14 -0700

    Restructure repository to match best practices (#42)
    * Restructure repo to have proper locations for package source and tests
    Port tests to pytest framework
    
    * Update chiltepin conda env to add pytest
    
    * Rework container to use user modifiable conda install
    
    Specify version 7.4.0 of pytest to work around bugs
    
    * Add pytest fixture to set up and teardown parsl for flux tests
    
    * Improve tests to check for output correctness
    
    * Fix bug, only remove previous output if it exists
    
    * Update CI workflow to use pytest
    
    * Remove unneeded test baseline output

---
+++ tests/test_parsl_hello.py
@@ -0,0 +1,40 @@
+import parsl
+from parsl.app.app import python_app, bash_app
+from parsl.configs.local_threads import config
+import pytest
+
+# Define a "hello world" Python Parsl App
+@python_app
+def python_hello (stdout=None, stderr=None):
+    return 'Hello World! from Python App'
+
+# Define a "hello world" Bash Parsl App
+@bash_app
+def bash_hello(stdout=None, stderr=None):
+    return 'echo "Hello World! from Bash App"'
+
+# Set up fixture to initialize and cleanup Parsl
+@pytest.fixture
+def load_config():
+    parsl.load(config)
+    yield
+    parsl.clear()
+
+# Test bash hello world
+def test_bash_hello(load_config):
+    assert bash_hello(stdout=('parsl_bash_hello.out', 'w'),
+                      stderr=('parsl_bash_hello.err', 'w')).result() == 0
+    baseline_stdout="Hello World! from Bash App\n"
+    baseline_stderr='''\
+--> executable follows <--
+echo "Hello World! from Bash App"
+--> end executable <--
+'''
+    with open("parsl_bash_hello.out", "r") as f:
+        assert f.read() == baseline_stdout
+    with open("parsl_bash_hello.err", "r") as f:
+        assert f.read() == baseline_stderr
+        
+# Test python hello world
+def test_python_hello(load_config):
+    assert python_hello().result() == "Hello World! from Python App"
