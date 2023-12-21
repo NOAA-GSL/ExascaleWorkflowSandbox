49590c417960ef609806ca15b46cea05ae7d42c6
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2023-12-20 18:42:09 -0700

    Add capability to use ghcr as a spack binary cache

--- docker/spack-stack/create_dockerfile.sh
+++ docker/spack-stack/create_dockerfile.sh
@@ -18,3 +18,14 @@ spack containerize > ../../../Dockerfile
 # Remove unneeded spack-stack install
 popd
 rm -rf spack-stack
+
+# Modify the Dockerfile to use buildcache mirror at our ghcr
+perl -p -i -e 's:cd /opt/spack-environment && spack env activate . && spack install --fail-fast && spack gc -y:foobar:g' Dockerfile
+perl -p -i -e 's:foobar:foobar1 \\\n&&  foobar2 \\\n&&  foobar3 \\\n&&  foobar4 \\\n&&  foobar5 \\\n&&  foobar6 \\\n&&  foobar7\n:g' Dockerfile
+perl -p -i -e 's:foobar1:--mount=type=secret,id=mirrors,target=/opt/spack/etc/spack/mirrors.yaml:g' Dockerfile
+perl -p -i -e 's:foobar2:cd /opt/spack-environment:s' Dockerfile
+perl -p -i -e 's:foobar3:\. \$SPACK_ROOT/share/spack/setup-env.sh:s' Dockerfile
+perl -p -i -e 's:foobar4:spack env activate \.:s' Dockerfile
+perl -p -i -e 's:foobar5:spack install --fail-fast --no-check-signature:s' Dockerfile
+perl -p -i -e 's:foobar6:spack buildcache push --unsigned ghcr_registry:s' Dockerfile
+perl -p -i -e 's:foobar7:spack gc -y:s' Dockerfile
