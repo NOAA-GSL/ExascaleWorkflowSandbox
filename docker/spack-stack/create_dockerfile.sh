20540a4e88a31dcf2c1152c07d525dda42d1cfcf
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2024-01-02 08:14:17 -0700

    Add capability to use AWS S3 bucket as a spack binary cache (#54)
    * Add capability to use ghcr as a spack binary cache
    
    * Add template for mirror config
    
    * Fix extreaneous && in Dockerfile generation
    
    * Add package write permissions to github token in CI
    
    * Try changing mirror url
    
    * Add contents: read permission
    
    * Increase resources for actions for spack-stack build
    
    * Update login action version and set logout to false
    
    * Try removing auth pair from mirror config
    
    * Add access pair back, use PAT as secret instead
    
    * Switch to AWS S3 build cache
    
    * Fix typo in workflow secret name
    
    * Revert back to standard github actions runner
    
    * Allow use of both read-only and read/write mirrors on S3
    
    This allows use of the read mirror when AWS S3 credentials
    are not available.
    
    Pushing to the rw mirror is done only if the secret mount
    for the rw mirror is present.

--- docker/spack-stack/create_dockerfile.sh
+++ docker/spack-stack/create_dockerfile.sh
@@ -12,9 +12,34 @@ spack stack create ctr --container=docker-ubuntu-gcc-openmpi --specs=jedi-ci
 cd envs/docker-ubuntu-gcc-openmpi
 perl -p -i -e "s/variants: \+internal-hwloc \+two_level_namespace/variants: \+internal-hwloc \+two_level_namespace \~static/g" spack.yaml
 
+# Modify spack.yaml to increase parallelism
+perl -p -i -e "s/build_jobs: 2/build_jobs: 16/g" spack.yaml
+
 # Create the spack-stack Dockerfile
 spack containerize > ../../../Dockerfile
 
 # Remove unneeded spack-stack install
 popd
 rm -rf spack-stack
+
+# Create spack build command patch
+export docker_patch=$(
+cat<<'END_HEREDOC'
+RUN --mount=type=secret,id=mirrors,target=/opt/spack/etc/spack/mirrors.yaml <<EOF
+  set -e
+  cd /opt/spack-environment
+  . $SPACK_ROOT/share/spack/setup-env.sh
+  spack env activate .
+  spack mirror add --s3-access-key-id "" --s3-access-key-secret "" s3_spack_stack_buildcache_ro s3://spack-stack-bulid-cache/ubuntu-x86
+  spack install --fail-fast --no-check-signature
+  spack mirror list
+  if [ "$(spack mirror list | wc -l)" = "3" ]; then
+    spack buildcache push --unsigned --update-index s3_spack_stack_buildcache_rw
+  fi
+  spack gc -y
+EOF
+END_HEREDOC
+)
+
+# Patch the Dockerfile to use buildcache mirrors on AWS S3
+perl -p -i -e 's:RUN cd /opt/spack-environment && spack env activate . && spack install --fail-fast && spack gc -y:$ENV{docker_patch}:g' Dockerfile
