85ed45e6f8bb177f5e1c415b2ed3dad42ddce966
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2024-01-08 10:49:34 -0700

    Feature/spack flux (#58)
    * Add initial install script
    
    * Remove comments
    
    * Update hera config to use locally installed spack-stack
    
    * Update hercules config to use locally installed spack-stack
    
    * Remove old conda install artifacts
    
    * Update test to account for differences in character sets on some machines
    
    * Update README with new instructions
    
    * Fix grammatical errors in README
    
    * Add use of buildcache to install script
    
    * Fix path to libpmi.so in config files
    
    * Fix grammer in README and install script

---
+++ install/install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+export SPACK_VERSION=develop
+
+# Install Spack via repository clone
+git clone -c feature.manyFiles=true -c core.sharedRepository=true https://github.com/spack/spack.git
+pushd spack
+git checkout ${SPACK_VERSION}
+popd
+
+# Initialize spack
+. spack/share/spack/setup-env.sh
+
+# Create and activate chiltepin environment
+spack env create chiltepin
+spack env activate chiltepin
+
+# Set up read-only mirror
+spack mirror add --s3-access-key-id "" --s3-access-key-secret "" s3_spack_stack_buildcache_ro s3://chiltepin/spack-stack/
+if [ -n "${S3_BUILD_CACHE_KEY}" ]; then
+  cat mirrors.in.yaml | envsubst > spack/etc/spack/mirrors.yaml
+  chmod 600 spack/etc/spack/mirrors.yaml
+fi
+spack mirror list
+
+# Add flux and pytest to chiltepin spack environment
+spack add python
+spack add py-pip
+spack add py-pytest
+spack add flux-core@0.53.0
+spack add flux-sched@0.28.0
+
+# Concretize and install the spack packages
+spack install --fail-fast --no-check-signature
+
+# Install parsl
+python -m pip install parsl[monitoring]==2023.12.4
+
+# Push the packages to the mirror
+if [ "$(spack mirror list | wc -l)" = "3" ]; then
+  export AWS_ACCESS_KEY_ID=${S3_BUILD_CACHE_KEY}
+  export AWS_SECRET_ACCESS_KEY=${S3_BUILD_CACHE_SECRET_KEY}
+  spack buildcache push --unsigned --update-index s3_spack_stack_buildcache_rw
+fi
