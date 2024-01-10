ffcd7de0ec0b72001995e68830c215b62049984d
NaureenBharwaniNOAA <136371446+NaureenBharwaniNOAA@users.noreply.github.com>
2024-01-10 14:35:47 -0600

    Update tests to test_run_mpi_pi verifies the tasks run concurrently (#57)
    * Initial work on testing validate output on mpi hello run
    
    * Debugged extra . in match statement
    
    * Initial time stamp addition
    
    * Update to time stamp
    
    * Fix mirror errors
    
    * Update time stamp
    
    * Remove repeated lines
    
    * Fix long line statements
    
    * Initial attempt at assertions
    
    * WIP assertions
    
    * Completed assertions
    
    * Fix typo in assertion
    
    * DRY out code

--- tests/mpi_pi.f90
+++ tests/mpi_pi.f90
@@ -30,7 +30,8 @@ program calculatePi
 
   if (worldRank == 0) then
     call date_and_time(VALUES=timeValues)
-    write(*,"(A,I2,A,I2,A,I2.2)") "Start Time = ", timeValues(5), ":", timeValues(6), ":", timeValues(7)
+    write(*,"(A,I2.2,A,I2.2,A,I4,A,I2.2,A,I2.2,A,I2.2)") "Start Time = ", timeValues(3), "/", timeValues(2), "/", timeValues(1), &
+            " ", timeValues(5), ":", timeValues(6), ":", timeValues(7)
   end if
 
   ! Initialize the random number seed
@@ -67,7 +68,8 @@ program calculatePi
 
   if (worldRank == 0) then
     call date_and_time(VALUES=timeValues)
-    write(*,"(A,I2,A,I2,A,I2.2)") "End Time = ", timeValues(5), ":", timeValues(6), ":", timeValues(7)
+    write(*,"(A,I2.2,A,I2.2,A,I4,A,I2.2,A,I2.2,A,I2.2)") "End Time = ", timeValues(3), "/", timeValues(2), "/", timeValues(1), & 
+            " ", timeValues(5), ":", timeValues(6), ":", timeValues(7)
   end if
 
   ! Finalize the MPI environment.
