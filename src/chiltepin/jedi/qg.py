04a875524c0ff99ea27b8b046a4735ad23171cf3
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2024-01-31 17:30:53 -0600

    Fix triple quote f-strings and add future dependencies for QG test

--- src/chiltepin/jedi/qg.py
+++ src/chiltepin/jedi/qg.py
@@ -3,6 +3,7 @@ import parsl
 from parsl.app.app import python_app, bash_app
 import pytest
 import re
+import textwrap
 
 class QG:
 
@@ -16,32 +17,28 @@ class QG:
 
     @bash_app(executors=['service'])
     def clone(env, install_path, tag="develop", stdout=None, stderr=None):
-        return '''
-        export INSTALL_PATH={}
-        export TAG={}
-        {}
-        hostname
+        return env + textwrap.dedent(f'''
+        echo Started at $(date)
+        echo Executing on $(hostname)
         git lfs install --skip-repo
-        rm -rf $INSTALL_PATH/jedi-bundle/$TAG/src
-        mkdir -p $INSTALL_PATH/jedi-bundle/$TAG
-        cd $INSTALL_PATH/jedi-bundle/$TAG
-        git clone --branch $TAG https://github.com/JCSDA/jedi-bundle.git src
-        '''.format(install_path, tag, env)
+        rm -rf {install_path}/jedi-bundle/{tag}/src
+        mkdir -p {install_path}/jedi-bundle/{tag}
+        cd {install_path}/jedi-bundle/{tag}
+        git clone --branch {tag} https://github.com/JCSDA/jedi-bundle.git src
+        echo Completed at $(date)
+        ''')
 
 
     @bash_app(executors=['service'])
-    def configure(env, install_path, tag="develop", stdout=None, stderr=None):
-        return '''
-        export INSTALL_PATH={}
-        export TAG={}
-        {}
-        hostname
-        cd $INSTALL_PATH/jedi-bundle/$TAG
+    def configure(env, install_path, tag="develop", stdout=None, stderr=None, clone=None):
+        return env + textwrap.dedent(f'''
+        echo Started at $(date)
+        echo Executing on $(hostname)
+        cd {install_path}/jedi-bundle/{tag}
         rm -rf build
         mkdir build
         cd build
-        pwd
-        ls ..
+        # Comment out the parts of the bundle we do not need
         perl -p -i -e "s/ecbuild_bundle\( PROJECT fv3/#ecbuild_bundle\( PROJECT fv3/g" ../src/CMakeLists.txt
         perl -p -i -e "s/ecbuild_bundle\( PROJECT femps/#ecbuild_bundle\( PROJECT femps/g" ../src/CMakeLists.txt
         perl -p -i -e "s/ecbuild_bundle\( PROJECT mom6/#ecbuild_bundle\( PROJECT mom6/g" ../src/CMakeLists.txt
@@ -50,18 +47,18 @@ class QG:
         perl -p -i -e "s/set\(MPAS/#set\(MPAS/g" ../src/CMakeLists.txt
         perl -p -i -e "s/ecbuild_bundle\( PROJECT mpas/#ecbuild_bundle\( PROJECT mpas/g" ../src/CMakeLists.txt
         ecbuild -DCMAKE_INSTALL_PREFIX=../ ../src
-        '''.format(install_path, tag, env)
+        echo Completed at $(date)
+        ''')
 
     @bash_app(executors=['serial'])
-    def make(env, install_path, tag="develop", stdout=None, stderr=None):
-        return '''
-        export INSTALL_PATH={}
-        export TAG={}
-        {}
-        hostname
+    def make(env, install_path, tag="develop", stdout=None, stderr=None, configure=None):
+        return env + textwrap.dedent(f'''
+        echo Started at $(date)
+        echo Executing on $(hostname)
         spack env list
         spack env deactivate
-        cd $INSTALL_PATH/jedi-bundle/$TAG/build
+        cd {install_path}/jedi-bundle/{tag}/build
         make -j16 VERBOSE=1
         make install
-        '''.format(install_path, tag, env)
+        echo Completed at $(date)
+        ''')
