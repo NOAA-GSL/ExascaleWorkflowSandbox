46abb65481f98007a3be1bbb7a1a50f01888eae3
Naureen <naureen.bharwani@noaa.gov>
2024-02-01 19:17:59 +0000

    Adding black check and diff options

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--config", action="store")
 
-
 @pytest.fixture(scope="session")
 def config_file(request):
     config_value = request.config.option.config
