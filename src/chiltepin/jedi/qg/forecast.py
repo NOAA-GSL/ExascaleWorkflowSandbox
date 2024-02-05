b6e824175382928603f1fcc8dcb2716b68341a91
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2024-02-05 11:26:37 -0600

    Simplify and generalize passing of config to forecast application

--- src/chiltepin/jedi/qg/forecast.py
+++ src/chiltepin/jedi/qg/forecast.py
@@ -13,14 +13,10 @@ def _merge_nested_dicts(d1, d2):
 
 
 @python_app(executors=['serial'])
-def _configure(workdir,
-               nx=40, ny=20,
-               dt="PT10M",
-               duration="P2D",
-               frequency="PT1H",
-               atime="2009-12-31T00:00:00Z",
-               background=None,
-               install=None):
+def _configure(rundir,
+               config,
+               install=None,
+               assimilation=None):
     # Get forecast default configuration file path
     config_file = os.path.dirname(__file__) + "/config/forecast.yaml"
 
@@ -28,72 +24,52 @@ def _configure(workdir,
     with open(config_file, "r") as stream:
         fcst_yaml = yaml.safe_load(stream)
 
-    # Create overriding config from options
-    options_yaml = yaml.load(textwrap.dedent(f"""
-    geometry:
-      nx: {nx}
-      ny: {ny}
-    output:
-      datadir: {workdir}
-    """).strip())
-
-    # Merge options into default config
-    _merge_nested_dicts(fcst_yaml, options_yaml)
-
-    # Turn on read from file is background is provided
-    if (background is not None):
-        fcst_yaml["initial condition"]["read_from_file"] = 1
+    # Merge input configuration
+    _merge_nested_dicts(fcst_yaml, config)
 
     # Make an empty forecast run directory
-    if (os.path.exists(workdir)):
-        shutil.rmtree(workdir)
-    os.makedirs(workdir)
+    if (os.path.exists(rundir)):
+        shutil.rmtree(rundir)
+    os.makedirs(rundir)
 
     # Dump the yaml config for input to forecast execution 
-    with open(f"{workdir}/truth.yaml", "w") as yaml_file:
+    with open(f"{rundir}/forecast.yaml", "w") as yaml_file:
         yaml.dump(fcst_yaml, yaml_file, default_flow_style=False)
 
     # Return the configuration string
     return fcst_yaml
 
 @bash_app(executors=['parallel'])
-def _execute(env, workdir, install_path, tag="develop", stdout=None, stderr=None, configure=None):
+def _execute(env, rundir, install_path, tag="develop", stdout=None, stderr=None, configure=None, assimilation=None):
     # Run the forecast executable
     return env + textwrap.dedent(f'''
     echo Started at $(date)
     echo Executing on $(hostname)
-    {install_path}/jedi-bundle/{tag}/build/bin/qg_forecast.x {workdir}/truth.yaml
+    {install_path}/jedi-bundle/{tag}/build/bin/qg_forecast.x {rundir}/forecast.yaml
     echo Completed at $(date)        
     ''')
 
 @join_app
 def run(env,
         install_path,
-        workdir,
-        nx=40, ny=20,
-        dt="PT10M",
-        duration="P2D",
-        frequency="PT1H",
-        atime="2009-12-31T00:00:00Z",
-        tag="develop",
-        stderr=None,
+        tag,
+        rundir,
+        config,
         stdout=None,
-        background=None,
-        install=None):
+        stderr=None,
+        install=None,
+        assimilation=None):
 
-    configure = _configure(workdir,
-                           nx=nx, ny=ny,
-                           dt=dt,
-                           duration=duration,
-                           frequency=frequency,
-                           atime=atime,
-                           background=background,
-                           install=install)
+    configure = _configure(rundir,
+                           config=config,
+                           install=install,
+                           assimilation=assimilation)
     execute = _execute(env,
-                       workdir,
+                       rundir,
                        install_path,
+                       tag=tag,
                        stdout=stdout,
                        stderr=stderr,
-                       tag=tag,
-                       configure=configure)
+                       configure=configure,
+                       assimilation=assimilation)
     return(execute)
