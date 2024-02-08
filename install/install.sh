a8cba0c35ee332c1c43fccfcdb43b51bfee1b494
Naureen <naureen.bharwani@noaa.gov>
2024-02-08 20:44:45 +0000

    Adding black to chiltepin env and container

--- install/install.sh
+++ install/install.sh
@@ -25,7 +25,7 @@ if [ -n "${S3_BUILD_CACHE_KEY}" ]; then
 fi
 spack mirror list
 
-# Add flux, pytest and flake8 to chiltepin spack environment
+# Add flux, pytest, flake8, and black to chiltepin spack environment
 spack add python
 spack add py-pip
 spack add py-pytest
@@ -33,6 +33,8 @@ spack add flux-core@0.53.0
 spack add flux-sched@0.28.0
 spack add py-flake8
 spack add py-pytest-flake8
+spack add py-black
+spack add py-pytest-black
 
 # Concretize and install the spack packages 
 spack install --fail-fast --no-check-signature
