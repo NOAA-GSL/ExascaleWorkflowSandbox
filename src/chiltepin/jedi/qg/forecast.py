306eaa13848bbfec3a0d87111ccf512e9e6ace58
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2024-03-05 12:26:20 -0600

    Refactor qg default config provisioning and add makeobs3d

--- src/chiltepin/jedi/qg/forecast.py
+++ src/chiltepin/jedi/qg/forecast.py
@@ -3,49 +3,39 @@ import os
 import shutil
 import textwrap
 import yaml
-
-def _merge_nested_dicts(d1, d2):
-    for key, value in d2.items():
-        if key in d1 and isinstance(d1[key], dict) and isinstance(value, dict):
-            _merge_nested_dicts(d1[key], value)
-        else:
-            d1[key] = value
-
+from chiltepin.jedi.qg.config import merge_config, forecast_default
 
 @python_app(executors=['serial'])
 def _configure(rundir,
                config,
                install=None,
                assimilation=None):
-    # Get forecast default configuration file path
-    config_file = os.path.dirname(__file__) + "/config/forecast.yaml"
+    # Set forecast configuration to the default
+    forecast_config = forecast_default()
 
-    # Load the base configuration
-    with open(config_file, "r") as stream:
-        fcst_yaml = yaml.safe_load(stream)
-
-    # Merge input configuration
-    _merge_nested_dicts(fcst_yaml, config)
+    # Merge input configuration overrides into default
+    merge_config(forecast_config, config)
 
     # Make an empty forecast run directory
     if (os.path.exists(rundir)):
         shutil.rmtree(rundir)
     os.makedirs(rundir)
 
-    # Dump the yaml config for input to forecast execution 
-    with open(f"{rundir}/forecast.yaml", "w") as yaml_file:
-        yaml.dump(fcst_yaml, yaml_file, default_flow_style=False)
+    # Dump the yaml config for input to forecast execution
+    config_filename = f"{rundir}/forecast.yaml"
+    with open(config_filename, "w") as yaml_file:
+        yaml.dump(forecast_config, yaml_file, default_flow_style=False)
 
-    # Return the configuration string
-    return fcst_yaml
+    # Return the configuration yaml
+    return config_filename
 
 @bash_app(executors=['parallel'])
-def _execute(env, rundir, install_path, tag="develop", stdout=None, stderr=None, configure=None, assimilation=None):
+def _execute(env, rundir, install_path, config_file, tag="develop", stdout=None, stderr=None, assimilation=None):
     # Run the forecast executable
     return env + textwrap.dedent(f'''
     echo Started at $(date)
     echo Executing on $(hostname)
-    {install_path}/jedi-bundle/{tag}/build/bin/qg_forecast.x {rundir}/forecast.yaml
+    {install_path}/jedi-bundle/{tag}/build/bin/qg_forecast.x {config_file}
     echo Completed at $(date)        
     ''')
 
@@ -67,9 +57,9 @@ def run(env,
     execute = _execute(env,
                        rundir,
                        install_path,
+                       config_file=configure,
                        tag=tag,
                        stdout=stdout,
                        stderr=stderr,
-                       configure=configure,
                        assimilation=assimilation)
     return(execute)
--- tests/test_qg.py
+++ tests/test_qg.py
@@ -6,39 +6,44 @@ import textwrap
 import yaml
 
 from chiltepin.config import factory, parse_file
-from chiltepin.jedi.qg import forecast, install
+from chiltepin.jedi.qg import install, forecast, hofx
+from chiltepin.jedi.qg.config import merge_config, forecast_default
 
-workdir = "/work/noaa/gsd-hpcs/charrop/hercules/SENA/ExascaleWorkflowSandbox.develop/tests"
+workdir = "/work/noaa/gsd-hpcs/charrop/hercules/SENA/ExascaleWorkflowSandbox.qg/tests"
 
 config_file = sys.argv[1]
 yaml_config = parse_file(config_file)
-config, environment = factory(yaml_config)
-parsl.load(config)
+resource_config, environment = factory(yaml_config)
+parsl.load(resource_config)
 
 # Install JEDI bundle
-install = install.run(environment,
-                      install_path=f"{workdir}",
-                      stdout=f"{workdir}/install.out",
-                      stderr=f"{workdir}/install.err",
-                      tag="develop")
+#install = install.run(environment,
+#                      install_path=f"{workdir}",
+#                      stdout=f"{workdir}/install.out",
+#                      stderr=f"{workdir}/install.err",
+#                      tag="develop")
 
-# Run a "truth" forecast
-truth = forecast.run(environment,
-                     install_path=f"{workdir}",
-                     tag="develop",
-                     rundir=f"{workdir}/experiments/QG/truth",
-                     config=yaml.safe_load(textwrap.dedent(f"""
-                     geometry:
-                         nx: 40
-                         ny: 20
-                     initial condition:
-                         read_from_file: 0
-                     output:
-                         datadir: {workdir}/experiments/QG/truth
-                     """).strip()),
-                     stdout=f"{workdir}/truth.out",
-                     stderr=f"{workdir}/truth.err",
-                     install=install)
+# Make the "truth" forecast configuration
+#truth_config = yaml.safe_load(textwrap.dedent(f"""
+#               geometry:
+#                   nx: 80
+#                   ny: 40
+#               initial condition:
+#                   read_from_file: 0
+#               output:
+#                   exp: truth
+#                   datadir: {workdir}/experiments/QG/truth
+#  
+#               """).strip())
+#
+#truth = forecast.run(environment,
+#                     install_path=f"{workdir}",
+#                     tag="develop",
+#                     rundir=f"{workdir}/experiments/QG/truth",
+#                     config=truth_config,
+#                     stdout=f"{workdir}/truth.out",
+#                     stderr=f"{workdir}/truth.err",
+#                     install=install)
 
 truth = None
 
@@ -49,12 +54,58 @@ obs = hofx.makeobs3d(environment,
                      rundir=f"{workdir}/experiments/QG/obs",
                      config=yaml.safe_load(textwrap.dedent(f"""
                      geometry:
-                     nx: 40
-                     ny: 20
+                       nx: 80
+                       ny: 40
+                       depths: [4500.0, 5500.0]
                      initial condition:
-                     read_from_file: 0
-                     output:
-                     datadir: {workdir}/experiments/QG/obs
+                       date: "2009-12-31T03:00:00Z"
+                       filename: {workdir}/experiments/QG/truth/truth.fc.2009-12-31T00:00:00Z.PT3H.nc
+                     model:
+                       name: QG
+                       tstep: PT10M
+                     forecast length: PT6H
+                     time window:
+                       begin: "2009-12-31T03:00:00Z"
+                       length: PT6H
+                     observations:
+                       observers:
+                       - obs operator:
+                           obs type: Stream
+                         obs space:
+                           obsdataout:
+                             obsfile: {workdir}/experiments/QG/obs/truth.obs3d.nc
+                           obs type: Stream
+                           generate:
+                             begin: PT2H
+                             nval: 1
+                             obs density: 100
+                             obs error: 4.0e6
+                             obs period: PT1H
+                       - obs operator:
+                           obs type: Wind
+                         obs space:
+                           obsdataout:
+                             obsfile: {workdir}/experiments/QG/obs/truth.obs3d.nc
+                           obs type: Wind
+                           generate:
+                             begin: PT3H
+                             nval: 2
+                             obs density: 100
+                             obs error: 6.0
+                             obs period: PT2H
+                       - obs operator:
+                           obs type: WSpeed
+                         obs space:
+                           obsdataout:
+                             obsfile: {workdir}/experiments/QG/obs//truth.obs3d.nc
+                           obs type: WSpeed
+                           generate:
+                             begin: PT4H
+                             nval: 1
+                             obs density: 100
+                             obs error: 12.0
+                             obs period: PT2H
+                     make obs: true
                      """).strip()),
                      stdout=f"{workdir}/makeob.out",
                      stderr=f"{workdir}/makeob.err",
@@ -63,3 +114,4 @@ obs = hofx.makeobs3d(environment,
 done = obs.result()
 
 parsl.clear
+
