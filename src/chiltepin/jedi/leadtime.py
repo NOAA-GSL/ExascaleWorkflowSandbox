825ac635488cc1a0ae3a33e912e8bbaec738e4a4
Naureen <naureen.bharwani@noaa.gov>
2024-03-28 19:31:32 +0000

    Update variable anme

--- src/chiltepin/jedi/leadtime.py
+++ src/chiltepin/jedi/leadtime.py
@@ -1,25 +1,25 @@
-def leadtime_to_seconds(fcst):
+def leadtime_to_seconds(leadtime):
     import re
 
     seconds = 0
     p = re.compile(r"\D+(\d+)S")
-    m = p.search(fcst)
+    m = p.search(leadtime)
     if m:
         seconds += int(m.group(1))
     p = re.compile(r"/\D+(\d+)M")
-    m = p.search(fcst)
+    m = p.search(leadtime)
     if m:
         seconds += int(m.group(1) * 60)
     p = re.compile(r"\D+(\d+)H")
-    m = p.search(fcst)
+    m = p.search(leadtime)
     if m:
         seconds += int(m.group(1)) * 3600
     p = re.compile(r"\D+(\d+)D")
-    m = p.search(fcst)
+    m = p.search(leadtime)
     if m:
         seconds += int(m.group(1)) * 3600 * 24
     p = re.compile("M")
-    m = p.match(fcst)
+    m = p.match(leadtime)
     if m:
         seconds = seconds * -1
     return seconds
@@ -27,23 +27,23 @@ def leadtime_to_seconds(fcst):
 
 def seconds_to_leadtime(s):
     seconds = s
-    fcst = ""
+    leadtime = ""
     if seconds < 0:
-        fcst = "M"
+        leadtime = "M"
     else:
-        fcst = "P"
+        leadtime = "P"
     days = seconds // (3600 * 24)
     if days > 0:
-        fcst = fcst + f"{days}D"
+        leadtime = leadtime + f"{days}D"
     if seconds % (3600 * 24) != 0 or seconds == 0:
-        fcst = fcst + "T"
+        leadtime = leadtime + "T"
     hours = (seconds - days * 3600 * 24) // 3600
     if hours > 0:
-        fcst = fcst + f"{hours}H"
+        leadtime = leadtime + f"{hours}H"
     minutes = (seconds - days * 3600 * 24 - hours * 3600) // 60
     if minutes > 0:
-        fcst = fcst + f"{minutes}M"
+        leadtime = leadtime + f"{minutes}M"
     seconds = seconds - days * 3600 * 24 - hours * 3600 - minutes * 60
     if (days == 0 and hours == 0 and minutes == 0) or (seconds > 0):
-        fcst = fcst + f"{seconds}S"
-    return fcst
+        leadtime = leadtime + f"{seconds}S"
+    return leadtime
