b6657b2b23ac4122218e358c77e7d480b397cf20
Naureen <naureen.bharwani@noaa.gov>
2024-03-28 19:35:08 +0000

    Ran black & isort

--- tests/test_leadtime.py
+++ tests/test_leadtime.py
@@ -1,5 +1,6 @@
 from chiltepin.jedi.leadtime import leadtime_to_seconds, seconds_to_leadtime
 
+
 def test_leadtime_to_seconds():
     assert leadtime_to_seconds("PT0S") == 0
     assert leadtime_to_seconds("PT1M") == 0
@@ -18,7 +19,7 @@ def test_leadtime_to_seconds():
     assert leadtime_to_seconds("M1DT1H") == -90000
     assert leadtime_to_seconds("M1DT1H30M") == -90000
     assert leadtime_to_seconds("M1DT1H30M") == -90000
-    
+
 
 def test_seconds_to_leadtime():
     pass
