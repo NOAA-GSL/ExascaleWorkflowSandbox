759579b2bd3037b6bf3f86e3099aca7722b77b6f
Naureen <naureen.bharwani@noaa.gov>
2024-03-28 17:12:36 +0000

    Add unit tests

--- tests/test_leadtime.py
+++ tests/test_leadtime.py
@@ -1,7 +1,24 @@
 from chiltepin.jedi.leadtime import leadtime_to_seconds, seconds_to_leadtime
 
 def test_leadtime_to_seconds():
-    pass
+    assert leadtime_to_seconds("PT0S") == 0
+    assert leadtime_to_seconds("PT1M") == 0
+    assert leadtime_to_seconds("PT1H") == 3600
+    assert leadtime_to_seconds("PT1H30M") == 3600
+
+    assert leadtime_to_seconds("P1D") == 86400
+    assert leadtime_to_seconds("P1DT1H") == 90000
+    assert leadtime_to_seconds("P1DT1H30M") == 90000
+
+    assert leadtime_to_seconds("MT0S") == -0
+    assert leadtime_to_seconds("MT1H") == -3600
+    assert leadtime_to_seconds("MT1H30M") == -3600
+
+    assert leadtime_to_seconds("M1D") == -86400
+    assert leadtime_to_seconds("M1DT1H") == -90000
+    assert leadtime_to_seconds("M1DT1H30M") == -90000
+    assert leadtime_to_seconds("M1DT1H30M") == -90000
+    
 
 def test_seconds_to_leadtime():
     pass
