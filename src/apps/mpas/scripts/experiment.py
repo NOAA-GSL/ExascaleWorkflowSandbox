a48fe1ba55be18c08e616052842f589da60e348d
Naureen Bharwani <naureenb@hercules-login-3.hpc.msstate.edu>
2024-07-03 12:20:59 -0500

    Experiment file update

--- src/apps/mpas/scripts/experiment.py
+++ src/apps/mpas/scripts/experiment.py
@@ -4,6 +4,7 @@ import parsl
 import pytest
 
 import chiltepin.configure
+import chiltepin.utils
 from chiltepin.mpas.wrapper import MPAS
 
 # Get resources and platform from command options
@@ -15,23 +16,24 @@ yaml_config = chiltepin.configure.parse_file(config_file)
 resources, environments = chiltepin.configure.factory(yaml_config, platform)
 environment = environments[platform]
 with parsl.load(resources):
-
-    # Instantiate MPAS object
-    mpas = MPAS(
-        environment=environment,
-        install_path="../",
-        tag="cbba5a4",
-    )
-
-    # Clone the MPAS repository
-    install_mpas = mpas.install(
-        #stdout=("install_mpas.out", "w"),
-        #stderr=("install_mpas.err", "w"),
-        stdout="install_mpas.out",
-        stderr="install_mpas.err",
-    )
-
-    install_mpas.result()
+    r = chiltein.utils.retrieve_data(stdout="get_ics.out", stderr="get_ics.err", ICS_or_LBCS="ICS", TIME_OFFSET_HRS=0, FCST_LEN=24, LBC_INTVL_HRS=6, YYYYMMDDHH="2024070300", OUTPUT_PATH=".")
+    r.result()
+#    # Instantiate MPAS object
+#    mpas = MPAS(
+#        environment=environment,
+#        install_path="../",
+#        tag="cbba5a4",
+#    )
+#
+#    # Clone the MPAS repository
+#    install_mpas = mpas.install(
+#        #stdout=("install_mpas.out", "w"),
+#        #stderr=("install_mpas.err", "w"),
+#        stdout="install_mpas.out",
+#        stderr="install_mpas.err",
+#    )
+#
+#    install_mpas.result()
 
 parsl.clear()
 
