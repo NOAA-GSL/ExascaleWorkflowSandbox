d542f35a3443135111d4709f63b3c6de0d6ba78d
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2024-09-05 08:45:23 -0600

    Fix some linting errors

--- tests/test_globus_compute_hello.py
+++ tests/test_globus_compute_hello.py
@@ -1,25 +1,45 @@
 import subprocess
 import os.path
 import shutil
-import pytest
 from globus_compute_sdk import Executor
+
 
 # Test Globus Compute Endpoint functionality
 def test_endpoint_config():
     shutil.rmtree("/home/admin/.globus_compute/foo", ignore_errors=True)
-    p = subprocess.run(["globus-compute-endpoint", "configure", "foo"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=20)
+    p = subprocess.run(
+        ["globus-compute-endpoint", "configure", "foo"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True, timeout=20
+    )
     assert p.returncode == 0
     assert os.path.exists("/home/admin/.globus_compute/foo/config.yaml")
 
+
 def test_endpoint_start():
-    p = subprocess.run(["globus-compute-endpoint", "start", "foo"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=20)
+    p = subprocess.run(
+        ["globus-compute-endpoint", "start", "foo"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        timeout=20
+    )
     assert p.returncode == 0
+
 
 def test_endpoint_hello():
     def hello():
         return "Hello"
 
-    p = subprocess.run("globus-compute-endpoint list | grep foo | cut -d' ' -f 2", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=20, shell=True)
+    p = subprocess.run(
+        "globus-compute-endpoint list | grep foo | cut -d' ' -f 2",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        timeout=20,
+        shell=True
+    )
     assert p.returncode==0
     hello_endpoint_id = p.stdout.strip()
     assert len(hello_endpoint_id) == 36
@@ -27,14 +47,25 @@ def test_endpoint_hello():
         future = gce.submit(hello)
         assert future.result() == "Hello"
 
+
 def test_endpoint_stop():
-    p = subprocess.run(["globus-compute-endpoint", "stop", "foo"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=20)
+    p = subprocess.run(
+        ["globus-compute-endpoint", "stop", "foo"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        timeout=20
+    )
     assert p.returncode == 0
+
 
 def test_endpoint_delete():
-    p = subprocess.run("echo y | globus-compute-endpoint delete foo", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=20, shell=True)
+    p = subprocess.run(
+        "echo y | globus-compute-endpoint delete foo",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        timeout=20,
+        shell=True
+    )
     assert p.returncode == 0
-
-
-    
-    
