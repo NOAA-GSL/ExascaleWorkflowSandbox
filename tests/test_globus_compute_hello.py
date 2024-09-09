f2d0b71798a0a79879b4b80aa4b86a3b1a088b5f
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2024-09-09 11:02:02 -0600

    Add globus compute endpoint configs and tests

--- tests/test_globus_compute_hello.py
+++ tests/test_globus_compute_hello.py
@@ -1,4 +1,5 @@
 import os.path
+import pathlib
 import shutil
 import subprocess
 
@@ -6,22 +7,23 @@ from globus_compute_sdk import Executor
 
 
 # Test Globus Compute Endpoint functionality
-def test_endpoint_config():
-    shutil.rmtree("/home/admin/.globus_compute/foo", ignore_errors=True)
-    p = subprocess.run(
-        ["globus-compute-endpoint", "configure", "foo"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-        timeout=20,
-    )
-    assert p.returncode == 0
-    assert os.path.exists("/home/admin/.globus_compute/foo/config.yaml")
+#def test_endpoint_config():
+#    shutil.rmtree("/home/admin/chiltepin/tests/globus_compute/foo", ignore_errors=True)
+#    p = subprocess.run(
+#        ["globus-compute-endpoint", "configure", "foo"],
+#        stdout=subprocess.PIPE,
+#        stderr=subprocess.STDOUT,
+#        text=True,
+#        timeout=20,
+#    )
+#    assert p.returncode == 0
+#    assert os.path.exists("/home/admin/.globus_compute/foo/config.yaml")
 
 
 def test_endpoint_start():
+    pwd = pathlib.Path(__file__).parent.resolve()
     p = subprocess.run(
-        ["globus-compute-endpoint", "start", "foo"],
+        ["globus-compute-endpoint", "-c", f"{pwd}/globus_compute", "start", "service"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
@@ -34,8 +36,9 @@ def test_endpoint_hello():
     def hello():
         return "Hello"
 
+    pwd = pathlib.Path(__file__).parent.resolve()
     p = subprocess.run(
-        "globus-compute-endpoint list | grep foo | cut -d' ' -f 2",
+        f"globus-compute-endpoint -c {pwd}/globus_compute list | grep service | cut -d' ' -f 2",
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
@@ -51,8 +54,9 @@ def test_endpoint_hello():
 
 
 def test_endpoint_stop():
+    pwd = pathlib.Path(__file__).parent.resolve()
     p = subprocess.run(
-        ["globus-compute-endpoint", "stop", "foo"],
+        ["globus-compute-endpoint", "-c", f"{pwd}/globus_compute", "stop", "service"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
@@ -61,13 +65,13 @@ def test_endpoint_stop():
     assert p.returncode == 0
 
 
-def test_endpoint_delete():
-    p = subprocess.run(
-        "echo y | globus-compute-endpoint delete foo",
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-        timeout=20,
-        shell=True,
-    )
-    assert p.returncode == 0
+#def test_endpoint_delete():
+#    p = subprocess.run(
+#        "echo y | globus-compute-endpoint delete foo",
+#        stdout=subprocess.PIPE,
+#        stderr=subprocess.STDOUT,
+#        text=True,
+#        timeout=20,
+#        shell=True,
+#    )
+#    assert p.returncode == 0
